d871764e57279d87243d2cb79c2c20981bdd619e
joaovfog <j.vitor48@outlook.com>
2023-02-28 08:47:44 -0300

    fix: general adjustments

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+export interface Course {
+  title: string;
+  date: string;
+}
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent {
-
+  courses: Course[] = [
+    { title: 'Certificado do Curso de Inglês na Wizard', date: 'mar 2015' },
+    { title: 'Angular Crash Course for Busy Developers', date: 'jun 2019' }
+  ]
 }
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+export interface Project {
+  title: string;
+  description: string;
+  link: string;
+}
+
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
 })
 export class ProjectsComponent {
-
+  projects: Project[] = [
+    {
+      title: 'Site Imobiliária',
+      description:
+        'Site para visualização, cadastro, edição e exclusão de imóveis (CRUD) utilizando Angular e Bootstrap.',
+      link: 'https://jfimoveisitapema.netlify.app/'
+    },
+    {
+      title: 'Dashboard',
+      description: 'Dashboard de vendas utilizando React e Tailwind.',
+      link: 'https://dashboard-design-example.netlify.app/'
+    },
+    {
+      title: 'Portfólio',
+      description: 'Portfólio pessoal utilizando HTML, CSS e JavaScript básico.',
+      link: 'https://jvf-portfolio.netlify.app/'
+    }
+  ];
 }
