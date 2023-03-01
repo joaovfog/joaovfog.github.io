3e7614d19360991d8a879009b789faa59d7a91dc
joaovfog <j.vitor48@outlook.com>
2023-03-01 20:46:57 -0300

    feat: dialog component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from './components/dialog/dialog.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   professionalExperiencesTitle = 'EXPERIÊNCIAS PROFISSIONAIS';
   skillsTitle = 'HABILIDADES';
   coursesTitle = 'CURSOS';
   projectsTitle = 'PROJETOS';
+
+  constructor(public dialog: MatDialog) {}
+
+  ngOnInit() {
+    this.dialog.open(DialogComponent)
+  }
 
   counter(count: number) {
     return count;
